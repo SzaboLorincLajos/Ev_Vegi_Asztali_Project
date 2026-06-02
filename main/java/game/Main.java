53e6dff723dd82a89c4bafb7864692966470c0c1
KristalyMate <kristaly.mate.2023@osztalyterem.katolikuskeri.hu>
2026-06-02 18:42:53 +0200

    TODO

--- main/java/game/Main.java
+++ main/java/game/Main.java
@@ -1,5 +1,9 @@
 package game;
 
+// TODO: A 2.0-s verzióban bevezetni a pénz (arany) és bolt rendszert
+// TODO: Átültetni a projektet konzolos felületről JavaFX ablakba
+// TODO: Bővíteni a SaveManager-t több mentési slot támogatására
+
 import game.ui.GameUI;
 
 public class Main {
