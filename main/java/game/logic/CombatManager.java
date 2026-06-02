c3d95400b7955c50604d918dc826b577bf67e7d0
SzaboLorincLajos <szabo.lorinc.lajos.2023@osztalyterem.katolikuskeri.hu>
2026-06-02 10:50:24 +0200

    ékezetek2

--- main/java/game/logic/CombatManager.java
+++ main/java/game/logic/CombatManager.java
@@ -17,45 +17,45 @@ public class CombatManager {
     }
 
     public boolean harc(Character jatekos, Enemy ellenfel) {
-        System.out.println("\nEllenfel jelenik meg: " + ellenfel.getNev());
-        System.out.println(ellenfel.getNev() + " eletero: " + ellenfel.getEletero());
+        System.out.println("\nEllenfél jelenik meg: " + ellenfel.getNev());
+        System.out.println(ellenfel.getNev() + " életerő: " + ellenfel.getEletero());
 
         while (jatekos.elHarcban() && ellenfel.elHarcban()) {
             System.out.println("\n-- A te korod --");
-            System.out.println("Eletero: " + jatekos.getEletero() + "/" + jatekos.getMaxEletero()
+            System.out.println("Életero: " + jatekos.getEletero() + "/" + jatekos.getMaxEletero()
                     + "  Mana: " + jatekos.getMana() + "/" + jatekos.getMaxMana()
-                    + "  Gyogyital: " + jatekos.getGyogyital());
-            System.out.println(ellenfel.getNev() + " eletero: " + ellenfel.getEletero());
-            System.out.println("Mit teszel? (1) tamadas  (2) varazslat  (3) gyogyital");
+                    + "  Gyógyital: " + jatekos.getGyogyital());
+            System.out.println(ellenfel.getNev() + " életerő: " + ellenfel.getEletero());
+            System.out.println("Mit teszel? (1) támadás  (2) varázslat  (3) gyógyital");
 
             String valasz = scanner.nextLine().trim();
 
             if (valasz.equals("1")) {
                 jatekos.tamad(ellenfel);
-                System.out.println("Tamadsz! Sebzés: " + jatekos.getSebzes());
+                System.out.println("Támadsz! Sebzés: " + jatekos.getSebzes());
             } else if (valasz.equals("2")) {
                 boolean siker = jatekos.varazslat(ellenfel);
                 if (siker) {
-                    System.out.println("Varazslat! Sebzés: " + (jatekos.getSebzes() * 2));
+                    System.out.println("Varázslat! Sebzés: " + (jatekos.getSebzes() * 2));
                 } else {
-                    System.out.println("Nincs eleg manad! (kell: 20)");
+                    System.out.println("Nincs elég manad! (kell: 20)");
                     continue;
                 }
             } else if (valasz.equals("3")) {
                 boolean siker = jatekos.gyogyit();
                 if (siker) {
-                    System.out.println("Gyogyitalt hasznaltál. Eletero: " + jatekos.getEletero());
+                    System.out.println("Gyógyitalt használtál. Életerő: " + jatekos.getEletero());
                 } else {
-                    System.out.println("Nincs tobbe gyogyitalod!");
+                    System.out.println("Nincs többé gyógyitalod!");
                     continue;
                 }
             } else {
-                System.out.println("Ervenytelen parancs. Irj 1, 2 vagy 3-at.");
+                System.out.println("Érvénytelen parancs. Írj 1, 2 vagy 3-at.");
                 continue;
             }
 
             if (!ellenfel.elHarcban()) {
-                System.out.println("\nLegyozted: " + ellenfel.getNev() + "!");
+                System.out.println("\nLegyőzted: " + ellenfel.getNev() + "!");
                 return true;
             }
 
@@ -63,7 +63,7 @@ public class CombatManager {
             int tenylegesSebzes = ellenfelSebzes - rand.nextInt(4);
             if (tenylegesSebzes < 1) tenylegesSebzes = 1;
             jatekos.sebzodes(tenylegesSebzes);
-            System.out.println(ellenfel.getNev() + " tamad! Kapsz " + tenylegesSebzes + " sebzest.");
+            System.out.println(ellenfel.getNev() + " támad! Kapsz " + tenylegesSebzes + " sebzést.");
 
             if (!jatekos.elHarcban()) {
                 System.out.println("\nMeghaltál a harcban...");
